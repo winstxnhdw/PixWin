5d678bff94737614c0130d74e8f648b2a6549057
winstxnhdw <winstxnhdw@gmail.com>
2022-11-23 19:50:27 +0800

    refactor: call get_rgb from libs module

--- pixwin/libs/__init__.py
+++ pixwin/libs/__init__.py
@@ -1,1 +1,3 @@
+from pixwin.libs.get_pixel import (get_blue_value, get_green_value,
+                                   get_red_value, get_rgb)
 from pixwin.libs.open_device_context import Win32OpenDC
