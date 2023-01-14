2e7a02a0d58319a456b5f7bd15b50ea49e153d09
winstxnhdw <winstxnhdw@gmail.com>
2023-01-14 21:38:45 +0800

    style: adhere to pylint

--- pixwin/__init__.py
+++ pixwin/__init__.py
@@ -7,9 +7,39 @@ from pixwin.libs import (Win32OpenDC, get_blue_value, get_green_value,
 
 
 class PixWin:
+    """
+    Summary
+    -------
+    Context manager for retrieving pixel values from a window.
 
+    Attributes
+    ----------
+    open_device_context (Win32OpenDC) : Win32OpenDC object used to open the device context
+    window_device_context_handle (int) : handle to the device context
+    gdi32 (WinDLL): gdi32 library.
+
+    Methods
+    -------
+    __init__(window_handle: int=0) -> self
+        initialise the PixWin object with a window handle
+
+    __enter__() -> self
+        open the device context and return the object
+
+    get_pixel(x: int, y: int) -> tuple[int, int, int]
+        get the RGB value of a pixel at a given x, y coordinate
+
+    get_red_value(x: int, y: int) -> int
+        get the red value of a pixel at a given x, y coordinate
+
+    get_green_value(x: int, y: int) -> int
+        get the green value of a pixel at a given x, y coordinate
+
+    get_blue_value(x: int, y: int) -> int
+        get the blue value of a pixel at a given x, y coordinate
+    """
     def __init__(self, window_handle: int=0):
-        
+
         self.open_device_context = Win32OpenDC(window_handle)
         self.window_device_context_handle = None
 
@@ -19,7 +49,7 @@ class PixWin:
 
 
     def __enter__(self) -> Self:
-        
+
         self.window_device_context_handle = self.open_device_context.__enter__()
         return self
 
@@ -30,20 +60,57 @@ class PixWin:
 
 
     def get_pixel(self, x: int, y: int) -> tuple[int, int, int]:
+        """
+        Summary
+        -------
+        Get the RGB value of a pixel at a given x, y coordinate.
 
+        Parameters
+        ----------
+        x (int) : x coordinate on the window
+        y (int) : y coordinate on the window
+        """
         return get_rgb(self.window_device_context_handle, x, y)
 
 
     def get_red_value(self, x: int, y: int) -> int:
+        """
+        Summary
+        -------
+        Get the red value of a pixel at a given x, y coordinate.
 
+        Parameters
+        ----------
+        x (int) : x coordinate on the window
+        y (int) : y coordinate on the window
+        """
         return get_red_value(self.window_device_context_handle, x, y)
 
 
     def get_green_value(self, x: int, y: int) -> int:
+        """
+        Summary
+        -------
+        Get the green value of a pixel at a given x, y coordinate.
 
+        Parameters
+        ----------
+        x (int) : x coordinate on the window
+        y (int) : y coordinate on the window
+        """
         return get_green_value(self.window_device_context_handle, x, y)
 
 
     def get_blue_value(self, x: int, y: int) -> int:
+        """
+        Summary
+        -------
+        Get the blue value of a pixel at a given x, y coordinate.
 
+        Parameters
+        ----------
+        x (int) : x coordinate on the window
+        y (int) : y coordinate on the window
+        """
         return get_blue_value(self.window_device_context_handle, x, y)
+    
--- pixwin/libs/open_device_context/__init__.py
+++ pixwin/libs/open_device_context/__init__.py
@@ -2,11 +2,29 @@ from ctypes import windll, wintypes
 
 
 class Win32OpenDC:
+    """
+    Summary
+    -------
+    Context manager for opening a device context.
 
+    Attributes
+    ----------
+    window_handle (int) : handle to the window
+    device_context_handle (int) : handle to the device context
+    user32 (WinDLL): user32 library
+
+    Methods
+    -------
+    __init__(window_handle: int) -> self
+        initialise the Win32OpenDC object with a window handle
+
+    __enter__() -> int
+        open the device context and return the handle
+    """
     def __init__(self, window_handle: int):
 
-        self.hWnd = window_handle
-        self.hDC = 0
+        self.window_handle = window_handle
+        self.device_context_handle = 0
 
         self.user32 = windll.user32
         self.user32.GetDC.argtypes     = [wintypes.HWND]
@@ -15,17 +33,17 @@ class Win32OpenDC:
         self.user32.ReleaseDC.restype  = wintypes.INT
 
 
-    def __enter__(self) -> int | None:
-        
-        self.hDC = self.user32.GetDC(self.hWnd)
+    def __enter__(self) -> int:
 
-        if self.hDC == 0:
+        self.device_context_handle = self.user32.GetDC(self.window_handle)
+
+        if self.device_context_handle == 0:
             raise WindowsError('[User32.dll] GetDC failed.')
 
-        return self.hDC
+        return self.device_context_handle
 
-    
+
     def __exit__(self, *_):
 
-        if self.user32.ReleaseDC(self.hWnd, self.hDC) == 0:
+        if self.user32.ReleaseDC(self.window_handle, self.device_context_handle) == 0:
             raise WindowsError('[User32.dll] ReleaseDC failed.')
