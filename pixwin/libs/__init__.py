9bd71ef22bf39d7588e388b4f4504840a7cc04d6
winstxnhdw <winstxnhdw@gmail.com>
2023-03-24 03:12:59 +0800

    style: use PEP 484 for re-exports

--- pixwin/libs/__init__.py
+++ pixwin/libs/__init__.py
@@ -1,3 +1,5 @@
-from pixwin.libs.get_pixel import (get_blue_value, get_green_value,
-                                   get_red_value, get_rgb)
-from pixwin.libs.open_device_context import Win32OpenDC
+from pixwin.libs.get_pixel import get_blue_value as get_blue_value
+from pixwin.libs.get_pixel import get_green_value as get_green_value
+from pixwin.libs.get_pixel import get_red_value as get_red_value
+from pixwin.libs.get_pixel import get_rgb as get_rgb
+from pixwin.libs.open_device_context import Win32OpenDC as Win32OpenDC
