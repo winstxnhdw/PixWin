a00c3593a1286dc22bc6762bbb22a5eb49374ee8
winstxnhdw <winstxnhdw@gmail.com>
2023-03-24 03:02:29 +0800

    style: use triple dots in stub files

--- pixwin/libs/get_pixel.pyi
+++ pixwin/libs/get_pixel.pyi
@@ -1,16 +1,13 @@
 # pylint: skip-file
+from pixwin.types import Blue, Green, Pixel, Red
 
-def get_rgb(device_context_handle: int, x: int, y: int) -> tuple[int, int, int]:
-    raise NotImplementedError
-
-
-def get_red_value(device_context_handle: int, x: int, y: int) -> tuple[int, int, int]:
-    raise NotImplementedError
+def get_rgb(device_context_handle: int, x: int, y: int) -> Pixel: ...
 
 
-def get_green_value(device_context_handle: int, x: int, y: int) -> tuple[int, int, int]:
-    raise NotImplementedError
+def get_red_value(device_context_handle: int, x: int, y: int) -> Red: ...
 
 
-def get_blue_value(device_context_handle: int, x: int, y: int) -> tuple[int, int, int]:
-    raise NotImplementedError
+def get_blue_value(device_context_handle: int, x: int, y: int) -> Blue: ...
+
+
+def get_green_value(device_context_handle: int, x: int, y: int) -> Green: ...
